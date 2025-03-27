cb5c055b5a2ddf57b0b182180d243c1528a74056
nathanaelsanilo <nathanaelsanilo@gmail.com>
2025-03-27 18:06:26 +0700

    feat: setup application properties

---
+++ src/main/java/com/nathan/belajar_springboot/config/ApplicationProperties.java
@@ -0,0 +1,40 @@
+package com.nathan.belajar_springboot.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "app")
+public class ApplicationProperties {
+
+    private String emailSmtp;
+
+    public String getEmailSmtp() {
+        return emailSmtp;
+    }
+
+    public void setEmailSmtp(String emailSmtp) {
+        this.emailSmtp = emailSmtp;
+    }
+
+    public String getEmailUser() {
+        return emailUser;
+    }
+
+    public void setEmailUser(String emailUser) {
+        this.emailUser = emailUser;
+    }
+
+    public String getEmailPassword() {
+        return emailPassword;
+    }
+
+    public void setEmailPassword(String emailPassword) {
+        this.emailPassword = emailPassword;
+    }
+
+    private String emailUser;
+
+    private String emailPassword;
+
+}
